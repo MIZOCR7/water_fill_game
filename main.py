b257625b0e03861a004713872cd8ae8b742662fa
MIZOCR7 <myfreelancinglife777@gmail.com>
2026-08-12 19:49:07 +0300

    finished the simplest UI

--- main.py
+++ main.py
@@ -12,20 +12,23 @@ pygame.display.set_caption('Water Fill Game')
 FPS = 60
 clock = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 24)
-new_game = True
 
 tubes = 10
 tube_colors = []
+color_choices = ['red', 'orange', 'light blue', 'dark blue', 'dark green', 'pink', 'purple', 'dark gray', 'brown', 'light green', 'yellow', 'white', 'gold', 'cyan'] 
 
 def main():
+  new_game = True
   while True:
     screen.fill('black') 
     clock.tick(FPS) 
     
     if new_game:
-      pass 
-    
-    
+      tubes, tube_colors = generate_start() 
+      initial_colors = copy.deepcopy(tube_colors) 
+      new_game = False 
+    else:
+      tube_rects = draw_tubes(tubes, tube_colors) 
     
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -38,12 +41,48 @@ def main():
 
 
 def generate_start():
-  tubes_number = random.randint(10, 14) 
+  tubes_number = random.randint(10, 12) 
   tube_colors = []
   available_colors = [] 
   
+  for i in range(tubes_number):
+    tube_colors.append([]) 
+    if i < tubes_number - 2:
+      for j in range(4):
+        available_colors.append(i) 
+        
+  for i in range(tubes_number - 2):
+    for j in range(4):
+      color = random.choice(available_colors) 
+      tube_colors[i].append(color) 
+      available_colors.remove(color)
+  
+  
   return tubes_number, tube_colors
 
+
+
+def draw_tubes(tubes_num, tube_cols):
+
+  tube_boxes = []
+  if tubes % 2 == 0:
+    tubes_per_row = tubes_num // 2
+    offset = False
+  else:
+    tubes_per_row = tubes_num // 2 + 1
+    offset = True
+
+  spacing = WIDTH / tubes_per_row
+
+  for i in range(tubes_per_row):
+    for j in range(len(tube_cols[i])):
+      pygame.draw.rect(screen, color_choices[tube_cols[i][j]], [5 + spacing*i, 200 - (50 * j), 65, 50], 0, 3)  
+
+    box = pygame.draw.rect(screen, 'blue', [5 + spacing*i, 50, 65, 200], 5, 3)
+
+    tube_boxes.append(box)  
+
+  return tube_boxes
 
    
 if __name__ == '__main__':
